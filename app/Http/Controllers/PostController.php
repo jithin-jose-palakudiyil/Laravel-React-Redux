bcff54b48a2e70490f2c33519488d89a25acfdf6
Jithin Jose <palakudiyil14@gmail.com>
2022-05-11 17:38:01 +0530

    basic completed

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json($posts);
+    }
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $post = Post::create($input);
+        return response()->json($post);
+    }
+
+    public function delete($post)
+    {
+        $deletedPost = Post::find($post)->delete();
+        return response()->json($deletedPost);
+    }
+}
